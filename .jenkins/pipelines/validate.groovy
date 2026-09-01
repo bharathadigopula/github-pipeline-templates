001c021d37effc1dc4205bca0d864ae1f78aba65
bharathadigopula <48708941+bharathadigopula@users.noreply.github.com>
2026-09-01 12:24:15 +0530

    feat: publish Jenkins validation status (#23)

--- .jenkins/pipelines/validate.groovy
+++ .jenkins/pipelines/validate.groovy
@@ -2,9 +2,10 @@
 // GITHUB PIPELINE TEMPLATE VALIDATION
 //==============================================================================
 
-@Library('jenkins-pipeline-templates@v1.3.0') _
+@Library('jenkins-pipeline-templates@v1.4.0') _
 
 repositoryValidationPipeline(
+    githubRepository: 'bharathadigopula/github-pipeline-templates',
     shellSearchPath: 'scripts',
     terraformDirectories: ['tests/fixtures/terraform-basic'],
     validationScript: 'tests/test-run-command-secret-order.sh',
